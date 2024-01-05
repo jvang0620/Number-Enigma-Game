f516d7b84401c71b35297355fdb05b0159b932e1
Jai_Vang <jaivang0387@gmail.com>
2024-01-05 09:59:59 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,111 @@
+﻿namespace Number_Enigma_Game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GetAppInfo();
+
+            GreetUser();
+
+            PlayGame();
+        }
+
+        static void GetAppInfo()
+        {
+            string appName = "Number Enigma Game";
+            string appVersion = "1.0.0";
+            string appAuthor = "Jai Vang";
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0}: Version {1} by {2} \n", appName, appVersion, appAuthor);
+            Console.ResetColor();
+        }
+
+        static void GreetUser()
+        {
+            Console.WriteLine("What is your name?");
+            string inputName = Console.ReadLine();
+            Console.WriteLine("");
+            Console.WriteLine("Hello {0}, let's play a game... \n", inputName);
+        }
+
+        static void PrintColorMessage(ConsoleColor color, string message)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        static void PlayGame()
+        {
+            while (true)
+            {
+                Random random = new Random();
+
+                // Generate a number from 1 to 10
+                int correctNumber = random.Next(1, 10);
+                int guessNumber = 0;
+                Console.WriteLine("Guess a number between 1 and 10 \n");
+
+                while (guessNumber != correctNumber)
+                {
+                    string input = Console.ReadLine();
+
+
+                    /*
+                    * This line checks if the input can be successfully converted to an integer. 
+                    * If not, it likely means that the input is not a valid numeric value, 
+                    * and the condition in the if statement will be true, 
+                    * allowing you to handle the case where the user entered an invalid input.
+                    */
+                    if (!int.TryParse(input, out guessNumber))
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "Please user an actual number. \n");
+
+                        // Keep going
+                        continue;
+                    }
+
+                    // Cast to int and put in guess variable
+                    guessNumber = Int32.Parse(input);
+
+                    // Match guess to correct number
+                    if (guessNumber != correctNumber)
+                    {
+                        PrintColorMessage(ConsoleColor.Red, "Wrong number! Please try again. \n");
+                    }
+                }
+
+                // Print success message
+                PrintColorMessage(ConsoleColor.Yellow, "You are CORRECT!!! \n");
+
+
+                // Ask to play again
+                var KeepLooping = true;
+                while (KeepLooping)
+                {
+                    Console.WriteLine("Play Again? [Y or N] \n");
+                    Console.WriteLine();
+                    string answer = Console.ReadLine().ToUpper();
+
+                    if (answer == "Y")
+                    {
+                        KeepLooping = false;
+                        continue;
+                    }
+                    else if (answer == "N")
+                    {
+                        Console.WriteLine("Thanks for playing! ByeByeBye! \n");
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter 'Y' or 'N' only \n");
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,57 @@
+# Number Enigma Game
+
+#### Overview
+
+A simple number guessing game where the user has to guess a random number between 1 and 10.
+
+#### Developed Using
+
+[![C#](https://img.shields.io/badge/language-C%23-blue.svg?style=for-the-badge)](https://docs.microsoft.com/en-us/dotnet/csharp/)
+
+[![ASP.NET Framework](https://img.shields.io/badge/ASP.NET-Framework-blueviolet.svg?style=for-the-badge)](https://dotnet.microsoft.com/apps/aspnet)
+
+#### Features
+
+- Randomly generated number for guessing.
+- User input validation for correct numeric guesses.
+- Play again functionality.
+
+#### How .NET is Used in Application
+
+- Developed using `C#`, a primary language supported by the .NET framework.
+- Employing .NET Core's `dotnet` commands for building and running the application.
+- Leveraging the `Visual Studio Community IDE`, providing the potential for future expansion into a web application.
+- Making use of `standard .NET libraries`, including `System`` namespaces. For instance:
+  - `System.Console` for handling console input/output.
+  - `System.Random` for generating random numbers.
+  - `System.Int32` for converting strings to integers.
+
+## Getting Started
+
+To run the Number Enigma Game on your local machine, please follow the steps below:
+
+1. Clone the repository to your local machine one of the following:
+   HTTPS: <pre><code>git clone https://github.com/jvang0620/Number-Enigma-Game.git</pre></code>
+   SSH: <pre><code>git clone git@github.com:jvang0620/Number-Enigma-Game.git</code></pre>
+
+2. Navigate to the project directory and type `cd Number-Enigma-Game`.
+
+3. Run the application by typing `dotnet run`.
+
+4. The program will display in the output. Happy Coding! :)
+
+#### Usage
+
+- Enter your name when prompted.
+- Guess a number between 1 and 10.
+- Follow the instructions for correct and incorrect guesses.
+- Choose to play again or exit the game.
+
+#### Game in Action
+
+![Screenshot of game #1](/images/image-1.PNG)
+![Screenshot of game #2](/images/image-2.PNG)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](https://opensource.org/license/mit/) file for details.
